3563f3bfbbd6c57787f7fa125822e7b6f7144ba9
Ibrahima Sylla <isyll711@gmail.com>
2023-11-25 14:06:15 +0000

    Add main module

---
+++ src/app/admin-hub/admin-hub.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminHubRoutingModule } from './admin-hub-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, AdminHubRoutingModule],
+})
+export class AdminHubModule {}
