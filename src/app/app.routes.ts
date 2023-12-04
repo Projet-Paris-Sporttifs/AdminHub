e66be5bab8b0a2e9f332bb5730ba38990a13032e
Ibrahima Sylla <isyll711@gmail.com>
2023-12-04 13:24:45 +0000

    create route for AdminHubModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./admin-hub/admin-hub.module').then((m) => m.AdminHubModule),
+  },
+  { path: '**', redirectTo: '' },
+];
