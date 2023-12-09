66b7ca21c8c35b7d4b7b32ea0db461faad26db30
Ibrahima Sylla <isyll711@gmail.com>
2023-12-09 19:52:33 +0000

    feat :sparkles: add theme switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core.module';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,18 @@ import { CoreModule } from './core/core.module';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'AdminHub';
+export class AppComponent implements OnInit {
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.theme$.subscribe((theme) => {
+      document.body.setAttribute('data-bs-theme', this.themeService.getTheme());
+    });
+  }
+
+  onSwitch(): void {
+    const newTheme =
+      this.themeService.getTheme() === 'light' ? 'dark' : 'light';
+    this.themeService.setTheme(newTheme);
+  }
 }
---
+++ src/app/core/services/theme.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private theme = new BehaviorSubject<string>('light');
+  theme$ = this.theme.asObservable();
+
+  constructor() {}
+
+  setTheme(theme: string): void {
+    this.theme.next(theme);
+  }
+
+  getTheme(): string {
+    return this.theme.value;
+  }
+}
